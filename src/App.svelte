a53f9e65607a73bf3eedc530449aadabdd570ba1
Jahiker <rojasjahiker@gmail.com>
2024-05-11 01:00:40 -0400

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+</script>
+
+<main>
+  <h1>Hello Pugstagram</h1>
+</main>
+
+<style>
+  main {
+    width: 100vw;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    min-height: 100vh;
+  }
+</style>
